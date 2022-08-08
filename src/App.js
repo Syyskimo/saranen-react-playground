b5859248dc2cc461182530fa96fa694b998e10d8
Mikko Salminen <mikko.salminen@louhosdigital.fi>
2022-08-08 14:00:25 +0300

    basic drink-stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header>
+        <div className="row">
+            <div className="logo">
+                <img id="logo" src="img/logo-drink.png" />
+            </div>
+            <div id="promile" class="promile">0.22</div>
+        </div>
+    </header>
+    );
+  }
 }
 
 export default App;
